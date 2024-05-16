674c2af057aca2dc49835fdb1b3aa2be78f0251b
Timok Alekseev <timofey.alekseev@list.ru>
2024-05-16 22:47:22 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,32 +1,74 @@
-function onClickButtonPlus(el) {
 
-    var firstvalue = document.querySelector('input[name="firstvalue"]').value;
-    var secondvalue = document.querySelector('input[name="secondvalue"]').value;
-    var firstnum = parseInt(firstvalue);
-    var secondnum = parseInt(secondvalue);
-    alert(firstnum + secondnum);
+var number = 0;
+var numbervideo = 0 ;
+function onClickButtonAddImage(el) {
+
+
+    if (numbervideo == 0) {
+        var newImage = document.createElement("img");
+        newImage.src = "../img/AK74.jpeg";
+        newImage.className = "image";
+        var targetElement = document.getElementById("image_target"); 
+        targetElement.appendChild(newImage);
+        number = number +1;
+    }
+
+    else if (number == 1) {
+        var secondImage = document.createElement("img");
+        secondImage.src = "../img/MP5.jpg"; 
+        secondImage.className = "image";
+        var targetElement = document.getElementById("image_target");
+        targetElement.appendChild(secondImage)
+        number = number +1;
+    }
+
+    else if (number == 2) {
+        var thirdImage = document.createElement("img");
+        thirdImage.src = "../img/M16A2.jpg"; 
+        thirdImage.className = "image";
+        var targetElement = document.getElementById("image_target");
+        targetElement.appendChild(thirdImage)
+        number = number +1;
+    }
+    else
+    {
+        var targetElement = document.getElementById("image_target");
+        var allimages = targetElement.querySelectorAll('ul > img');
+        allimages.forEach(function (image){
+            image.remove(); 
+        });
+        number = 0;
+    }
+
 }
 
-function onClickButtonMinus(el) {
-    var firstvalue = document.querySelector('input[name="firstvalue"]').value;
-    var secondvalue = document.querySelector('input[name="secondvalue"]').value;
-    var firstnum = parseInt(firstvalue);
-    var secondnum = parseInt(secondvalue);
-    alert(firstnum - secondnum);
-}
+function OnClickButtonAddVideo(el){
+     if (numbervideo == 0) {
+        var newvideo = document.createElement("video");
+        newvideo.src = "../video/Original Vietnam-Era M60 at the Range.mp4";
+        var targetElement = document.getElementById("video_target"); 
+        newvideo.autoplay = true;
+        targetElement.appendChild(newvideo);
+        numbervideo = number +1;
+    }
 
-function onClickButtonMultiply(el){
-	    var firstvalue = document.querySelector('input[name="firstvalue"]').value;
-    var secondvalue = document.querySelector('input[name="secondvalue"]').value;
-    var firstnum = parseInt(firstvalue);
-    var secondnum = parseInt(secondvalue);
-    alert(firstnum * secondnum);
-}
+    else if (numbervideo == 1) {
+        var secondVidoe = document.createElement("video");
+        secondVidoe.src = "../video/Shooting a Suppressed Sten Gun.mp4";
+        var targetElement = document.getElementById("video_target"); 
+        secondVidoe.autoplay = true;
+        targetElement.appendChild(secondVidoe);
+        numbervideo = numbervideo +1;
+    }
 
-function onClickButtonDel(el){
-	    var firstvalue = document.querySelector('input[name="firstvalue"]').value;
-    var secondvalue = document.querySelector('input[name="secondvalue"]').value;
-    var firstnum = parseInt(firstvalue);
-    var secondnum = parseInt(secondvalue);
-    alert(firstnum / secondnum);
+    else
+    {
+        var targetElement = document.getElementById("video_target");
+        var allvideo = targetElement.querySelectorAll('ul > video');
+        allvideo.forEach(function (video){
+            video.remove(); 
+        });
+        numbervideo = 0;
+    }
+
 }
